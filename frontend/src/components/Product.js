bc5bd549eb25a209fbae8cdb3cd3c4a936eea250
scottwcode <scottmail77@gmail.com>
2020-10-08 17:16:50 -0400

    After 7-Rating Component before implementing react router

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
+import Rating from './Rating';
 
 const Product = ({ product }) => {
   return (
@@ -7,17 +8,22 @@ const Product = ({ product }) => {
       <a href={`/product/${product._id}`}>
         <Card.Img src={product.image} variant="top" />
       </a>
+
       <Card.Body>
         <a href={`/product/${product._id}`}>
           <Card.Title as="div">
             <strong>{product.name}</strong>
           </Card.Title>
         </a>
+
         <Card.Text as="div">
-          <div className="my-3>">
-            {product.rating} from {product.numReviews} reviews
-          </div>
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+            // color="red"
+          />
         </Card.Text>
+
         <Card.Text as="h3">${product.price}</Card.Text>
       </Card.Body>
     </Card>
