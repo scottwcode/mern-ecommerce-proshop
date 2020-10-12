9ca46849041b6bc6a790f16178c9f4bdd79334d8
scottwcode <scottmail77@gmail.com>
2020-10-12 16:21:58 -0400

    After creating user authentication Endpoint 37

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,9 @@ import dotenv from 'dotenv';
 import colors from 'colors';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
-// import products from './data/products.js';
 
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -13,21 +13,16 @@ connectDB();
 
 const app = express();
 
-// // Section for Testing for middleware setup
-// app.use((req, res, next) => {
-//   // console.log('HELLO');
-//   console.log('original URL = ', req.originalUrl);
-//   next();
-// });
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
-
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
