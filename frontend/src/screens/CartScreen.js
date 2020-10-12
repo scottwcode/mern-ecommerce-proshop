8c46ed0016d9d2d06ae25ed30a909f0a1fafc381
scottwcode <scottmail77@gmail.com>
2020-10-12 15:33:49 -0400

    After coding remove items from cart 35 - end of section 6 - adding the shopping cart

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -11,7 +11,7 @@ import {
   Card,
 } from 'react-bootstrap';
 import Message from '../components/Message';
-import { addToCart } from '../actions/cartActions';
+import { addToCart, removeFromCart } from '../actions/cartActions';
 
 const CartScreen = ({ match, location, history }) => {
   const productId = match.params.id;
@@ -30,11 +30,11 @@ const CartScreen = ({ match, location, history }) => {
   }, [dispatch, productId, qty]);
 
   const removeFromCartHandler = (id) => {
-    history.push('/login?redirect=shipping');
+    dispatch(removeFromCart(id));
   };
 
   const checkoutHandler = () => {
-    console.log('checkout');
+    history.push('/login?redirect=shipping');
   };
 
   return (
