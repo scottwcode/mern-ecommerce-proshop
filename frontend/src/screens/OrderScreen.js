77101d8106e205c5e68597ce90e85a981a7d7415
scottwcode <scottmail77@gmail.com>
2020-10-14 11:02:06 -0400

    Added Order Pay reducer and action 61

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -34,7 +34,7 @@ const OrderScreen = ({ match }) => {
     if (!order || order._id !== orderId) {
       dispatch(getOrderDetails(orderId));
     }
-  }, [order, orderId]);
+  }, [dispatch, orderId]);
 
   return loading ? (
     <Loader />
