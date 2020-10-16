c050fd8a01aa4de2902704e7e79a79b3d611881f
scottwcode <scottmail77@gmail.com>
2020-10-15 21:42:05 -0400

    coded Admin Product List and backend part of admin delete products

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -24,4 +24,20 @@ const getProductById = asyncHandler(async (req, res) => {
   }
 });
 
-export { getProducts, getProductById };
+// @desc Delete a product
+// @route DELETE /api/product/:id
+// @access Private/Admin
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    // Any Admin can delete any product, not just the ones they created
+    await product.remove();
+    res.json({ message: 'Product removed' });
+  } else {
+    res.status(404);
+    throw new Error('Product not Found');
+  }
+});
+
+export { getProducts, getProductById, deleteProduct };
