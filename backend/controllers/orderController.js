698f6b4cfedd6595f39654e8bf211dc1e42a358f
scottwcode <scottmail77@gmail.com>
2020-10-17 09:09:11 -0400

    Created Admin Order list page for an Admin to view any order

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -84,8 +84,6 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
   }
 });
 
-// export { addOrderItems, getOrderById, updateOrderToPaid };
-
 // @desc    Get logged in user orders
 // @route   GET /api/orders/myorders
 // @access  Private
@@ -94,4 +92,18 @@ const getMyOrders = asyncHandler(async (req, res) => {
   res.json(orders);
 });
 
-export { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders };
+// @desc    Get all orders
+// @route   GET /api/orders
+// @access  Private/Admin
+const getOrders = asyncHandler(async (req, res) => {
+  const orders = await Order.find({}).populate('user', 'id name');
+  res.json(orders);
+});
+
+export {
+  addOrderItems,
+  getOrderById,
+  updateOrderToPaid,
+  getMyOrders,
+  getOrders,
+};
